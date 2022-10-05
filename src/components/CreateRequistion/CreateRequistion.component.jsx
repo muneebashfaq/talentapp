0474335a7d6624988db0b9abaee841c124926713
Muhammad Muneeb <mmuneeb840@gmail.com>
2022-10-05 15:44:23 +0500

    deployed

--- src/components/CreateRequistion/CreateRequistion.component.jsx
+++ src/components/CreateRequistion/CreateRequistion.component.jsx
@@ -1,8 +1,77 @@
 import './CreateRequistion.styles.css'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 const CreateRequistion=()=>{
+    const [age, setAge] = React.useState('');
+
+    const handleChange = (event) => {
+      setAge(event.target.value);
+    };
     return(<>
     <div className="createrequistion_body">
-        <h1>This will be requistion Create page</h1>
+        <div className='row'>
+<h2>Create New Requisition</h2>
+        </div>
+        <hr/>
+        <div className='row'>
+            <div className='col-xl-12'>
+            <div>
+      <FormControl sx={{ m: 1, minWidth: 980 }}>
+        <InputLabel id="demo-simple-select-autowidth-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-autowidth-label"
+          id="demo-simple-select-autowidth"
+          value={age}
+          onChange={handleChange}
+          autoWidth
+          label="Age"
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Twenty</MenuItem>
+          <MenuItem value={21}>Twenty one</MenuItem>
+          <MenuItem value={22}>Twenty one and a half</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+            </div>
+
+        </div>
+      
+       <div className='row'>
+        <div className='col-xl-6'>
+        <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '45ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+    </Box>
+        </div>
+        <div className='col-xl-6'>
+        <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '45ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+    </Box>
+        </div>
+
+       </div>
     </div>
     </>)
 }
